db29ce086decf2bb3cfd33115f087c24555a7ee2
Brandi Beaudoin <brandi.beaudoin98@gmail.com>
2023-09-29 22:03:39 -0700

    Updated index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost::27017/myFlixDB', {
+mongoose.connect('mongodb://127.0.0.1:27017/cfDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
