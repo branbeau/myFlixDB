960348db7f6dfd1739c42ec720b45d001009e11a
Brandi Beaudoin <brandi.beaudoin98@gmail.com>
2023-10-02 22:20:29 -0700

    Updated users.js

--- users.js
+++ users.js
@@ -1,38 +1,39 @@
-{
-  Username: "gabbybeau123",
-  Password: "test456",
-  Email:"gabby@gmail.com",
-  Birthday: "11/01/1990",
-  FavoriteMovies: [ 1, 3, 5 ]
-}, 
+const users = [
+  {
+    Username: "gabbybeau123",
+    Password: "test456",
+    Email: "gabby@gmail.com",
+    Birthday: "11/01/1990",
+    FavoriteMovies: [ 1, 3, 5 ]
+  }, 
+  {
+    Username: "millie567",
+    Password: "test789",
+    Email:"millie@gmail.com",
+    Birthday: "05/30/1872",
+    FavoriteMovies: [ 4, 2, 1 ]
+  },
+  {
+    Username: "branbeau891",
+    Password: "test101",
+    Email:"bran@gmail.com",
+    Birthday: "01/21/1990",
+    FavoriteMovies: [ 8, 10, 6 ]
+  }, 
+  {
+    Username: "barb102",
+    Password: "test417",
+    Email:"barb@gmail.com",
+    Birthday: "04/15/1950",
+    FavoriteMovies: [ 7, 3, 5 ]
+  },
+  {
+    Username: "will777",
+    Password: "test891",
+    Email:"will@gmail.com",
+    Birthday: "08/01/1960",
+    FavoriteMovies: [ 10, 4, 5 ]
+  }
+];
 
-{
-  Username: "millie567",
-  Password: "test789",
-  Email:"millie@gmail.com",
-  Birthday: "05/30/1872",
-  FavoriteMovies: [ 4, 2, 1 ]
-
-{
-  Username: "branbeau891",
-  Password: "test101",
-  Email:"bran@gmail.com",
-  Birthday: "01/21/1990",
-  FavoriteMovies: [ 8, 10, 6 ]
-}, 
-
-{
-  Username: "barb102",
-  Password: "test417",
-  Email:"barb@gmail.com",
-  Birthday: "04/15/1950"
-  FavoriteMovies: [ 7, 3, 5 ]
-},
-
-{
-  Username: "will777",
-  Password: "test891",
-  Email:"will@gmail.com",
-  Birthday: "08/01/1960"
-  FavoriteMovies: [ 10, 4, 5 ]
-}
+console.log(users);
