0009837bbf7de45415b212104326512c94d2df1e
Brandi Beaudoin <brandi.beaudoin98@gmail.com>
2023-10-08 14:28:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -78,11 +78,10 @@ const movies = require('./exported_collections/movies.json');
 const users = require('./exported_collections/users.json');
 
 //Get all movies
-app.get('/movies', (req, res) => {
 app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
   Movies.find()
     .then((movies) => {
-      res.status(201).json(movies);
+      res.status(200).json(movies);
     })
     .catch((err) => {
       console.error(err);
