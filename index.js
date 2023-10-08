c5832b8bb6976f9781cabe3d018b930e7cd7305c
Brandi Beaudoin <brandi.beaudoin98@gmail.com>
2023-10-08 14:18:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const movieSchema = new mongoose.Schema({
 
 const dbName = 'cfDB';
 
-const url = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/' + dbName;
+const url = process.env.CONNECTION_URI || 'mongodb://127.0.0.1:27017/' + dbName;
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
