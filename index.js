1045180eeb860cce7b7795490ee7c43c90c17519
Brandi Beaudoin <brandi.beaudoin98@gmail.com>
2023-10-10 00:16:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -35,30 +35,6 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 // Get the MongoDB connection URI from an environment variable
 const uri = process.env.MONGODB_URI;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-
-run().catch(console.dir);
-
 let allowedOrigins = [
   "https://myflixapp-56b818d4e5ca.herokuapp.com",
   "http://localhost:1234",
