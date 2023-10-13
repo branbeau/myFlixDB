7b20875bafe57d50b9d1d41af7662de68f321515
Brandi Beaudoin <brandi.beaudoin98@gmail.com>
2023-10-12 21:47:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -30,9 +30,10 @@ require('./passport');
 
 const dbName = 'cfDB';
 
-const url = process.env.CONNECTION_URI || 'mongodb://127.0.0.1:27017/' + cfDB;
-//mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-//mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true, autoIndex: true});
+mongoose.connect(process.env.CONNECTION_URI || 'mongodb://127.0.0.1:27017/' + cfDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+//const url = process.env.CONNECTION_URI || 'mongodb://127.0.0.1:27017/' + cfDB;
+//mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Get the MongoDB connection URI from an environment variable
 const uri = process.env.MONGODB_URI;
